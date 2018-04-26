a885a7bee554ff4b4f4106f491bf241533ecc7dd
Damadj <Damadj2@gmail.com>
2018-04-26 23:47:51 +0500

    Работа с условиями

--- index.php
+++ index.php
@@ -96,8 +96,7 @@ $show_complete_tasks = rand(0, 1);
                     </nav>
 
                     <label class="checkbox">
-                        <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1) {echo("checked"); } ?> >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -117,7 +116,25 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                    <?php
+                      $task = '<tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                          <label class="checkbox task__checkbox">
+                            <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                          </label>
+                        </td>
+                        <td class="task__date">10.04.2017</td>
+
+                        <td class="task__controls">
+                        </td>
+                      </tr>';
+                      if ($show_complete_tasks == 1) {
+                        echo($task);
+                    }
+                    ?>
+
+
                     <tr class="tasks__item task task--completed">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
