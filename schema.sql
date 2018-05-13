70558c0bf1d8ab7e8296818b3eedb0b166211499
Damadj <Damadj2@gmail.com>
2018-05-13 22:02:21 +0500

    Создание схемы БД личного проекта #1

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE doingsdone
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+  CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project CHAR(128),
+    user_id INT
+  );
+  CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    creation_date INT,
+    completion_date INT,
+    task_name CHAR(128),
+    file_link CHAR(128),
+    time_limit INT,
+    user_id INT,
+    project_id INT
+  );
+  CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    registration_date INT,
+    email CHAR(128),
+    user_name CHAR(64),
+    password CHAR(64),
+    contact CHAR(128)
+  );
+  CREATE INDEX project ON categories(project);
+  CREATE UNIQUE INDEX email ON users(email);
+  CREATE UNIQUE INDEX user_name ON users(user_name);
